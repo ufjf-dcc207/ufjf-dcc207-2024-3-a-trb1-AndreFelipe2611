57edae0e1fc750fa421a6856e58c3dbf3a5f2e46
André Felipe <andre1104mg@gmail.com>
2024-12-14 01:35:03 -0300

    css arrumado

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import Podium from "./Podium";
 function App() {
   return (
     <section className="podium">
-      <Podium position={1} image="game-a.jpg" text="Game A" title="Game A" />
-      <Podium position={2} image="game-b.jpg" text="Game B" title="Game B" />
-      <Podium position={3} image="game-c.jpg" text="Game C" title="Game C" />
+      <Podium position={2} top={2} image="game-a.jpg" text="Game A" title="Game A" />
+      <Podium position={1} top={1} image="game-b.jpg" text="Game B" title="Game B" />
+      <Podium position={3} top={3} image="game-c.jpg" text="Game C" title="Game C" />
     </section>
   );
 }
--- src/Podium.tsx
+++ src/Podium.tsx
@@ -2,15 +2,19 @@ import "./Podium.css";
 
 interface PodiumProps {
     position: number;
+    top: number;
     image: string;
     text: string;
     title: string;
 }
 
-export default function Podium({ position, image, text, title }: PodiumProps) {
+export default function Podium({ position, top, image, text, title }: PodiumProps) {
+  
+    const className = position === 1 ? "first" : position === 2 ? "second" : "third";
     return (
-        <div className={`place ${position === 1 ? "first" : position === 2 ? "second" : "third"}`}>
-            <span>{`${position}º`}</span>
+        <div className={`place ${className}`}>
+            <span>{top}</span>
+            <span>{position}</span>
             <img src={image} alt={text} />
             <h2>{title}</h2>
         </div>
