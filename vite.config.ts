70c2be6d0554148156da1a3a81e3b29f4c038ed3
André Felipe <112342453+AndreFelipe2611@users.noreply.github.com>
2024-12-16 17:20:43 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/ufjf-dcc207-2024-3-a-atvc01-AndreFelipe",
+  base: "/ufjf-dcc207-2024-3-a-trb1-AndreFelipe2611",
   plugins: [react()],
 })
